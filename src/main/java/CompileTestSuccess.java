b8b0702a63dfb448296402a8d1c99edd91146be4
linopuser <georgewba@gmail.com>
2020-02-05 15:58:53 +0100

    added files that compiles and have successful tests

---
+++ src/main/java/CompileTestSuccess.java
@@ -0,0 +1,11 @@
+public class CompileTestSuccess {
+
+    public static void main(String[] args) {
+        CompileTestSuccess cts = new CompileTestSuccess();
+        cts.getTrue();
+    }
+
+    public boolean getTrue(){
+        return true;
+    }
+}
